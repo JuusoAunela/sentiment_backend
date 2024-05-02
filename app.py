8e3a8ee6f7da58cf910d7308c32d22a394eb6e6a
Juuso Aunela <juusoaunela@gmail.com>
2024-05-02 21:56:26 +0300

    comments

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ import sentiment_analysis as sa
 
 app = Flask(__name__)
 
+# Test route
 @app.route('/')
 def index():
     return 'Hello World!'
 
+# Return sentiment analysis result
 @app.route('/sentiment', methods=['POST'])
 def sentiment():
+    # get sentence from request
     sentence = request.json['sentence']
 
+    # check if sentence is valid
     if sentence and len(sentence) > 0:
         return sa.get_sentiment(sentence)
+    # else return error
     else:
-        return 'Negative'
+        # TODO: handle error
+        return 'Error: Invalid sentence'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,15 +1,23 @@
 from textblob import TextBlob
 
+# sentiment analysis from previous task assignment
+
+# Analyze the polarity of the sentence
 def get_polarity(text):
   return str(TextBlob(text).sentiment.polarity)
 
+# Analyze the subjectivity of the sentence
 def get_subjectivity(text):
   return str(TextBlob(text).sentiment.subjectivity)
 
+# Analyze the sentiment of the sentence
 def get_sentiment(text):
+  # Sentence is positive
   if TextBlob(text).sentiment.polarity > 0.25:
     return "Positive"
+  # Sentence is negative
   elif TextBlob(text).sentiment.polarity < -0.25:
     return "Negative"
+  # Sentence is neutral
   else:
     return "Neutral"
