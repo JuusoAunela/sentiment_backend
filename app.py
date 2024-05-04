0e145ef981d3c403fb49ae3ada9258d9557fb7f3
Juuso Aunela <juusoaunela@gmail.com>
2024-05-04 22:40:28 +0300

    Change CORS off

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import sentiment_analysis as sa
 
 app = Flask(__name__)
 
+# Set CORS off
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 # Test route
 @app.route('/')
 def index():
